da98e1e5b925075da4d935feb671a5ca22814777
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2023-12-31 12:15:05 +0100

    Update NN_upload_and_traning.py

--- NN_upload_and_traning.py
+++ NN_upload_and_traning.py
@@ -42,9 +42,9 @@ if uploaded_file is not None:
     # Quest'ultimo verrà modificato a seconda delle scelte dell'utente
     # Leggo il dataframe con separatore ;
     # Se il separatore è diverso, uso un if per ri-leggerlo.
-    dataframe = pd.read_csv(uploaded_file, delimiter=";")
-    if len( dataframe.columns ) == 1 :
-        dataframe = pd.read_csv(uploaded_file, delimiter=",")
+    dataframe = pd.read_csv(uploaded_file, delimiter=",")
+    #if len( dataframe.columns ) == 1 :
+    #    dataframe = pd.read_csv(uploaded_file, delimiter=",")
         
     if st.checkbox('Show dataframe', key=50):                
         st.write(dataframe)
