9638d6235eb50514a9e45d0c2b86ab5a806022ec
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2023-12-31 12:03:24 +0100

    Update Function_Metrics_plot.py

--- functions/Function_Metrics_plot.py
+++ functions/Function_Metrics_plot.py
@@ -53,6 +53,8 @@ def Metrics_plot(Model, X_train, X_test, y_train, y_test, Task, Norm_tar_list) :
             ax2.plot( 10**Model.Predict(X_test)+1, 10**(y_test)+1, 'ob')
             rangg = np.arange( (10**Model.Predict(X_test) +1 ).min(), (10**Model.Predict(X_test)).max())
             ax3.hist(10**Model.Predict(X_test) - 10**(y_test), bins = 20, color = 'blue', alpha = 0.7)
+
+            st.write(10**Model.Predict(X_test)+1, 10**(y_test)+1)
         else :
             ax2.plot(Model.Predict(X_test),y_test, 'ob')
             rangg = np.arange(Model.Predict(X_test).min(), Model.Predict(X_test).max())
