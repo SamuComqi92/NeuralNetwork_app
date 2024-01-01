09730d085495189efbd3b0a604fda72e9f8179a5
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-01 14:57:33 +0100

    Update NeuralNetwork.py

--- model/NeuralNetwork.py
+++ model/NeuralNetwork.py
@@ -302,7 +302,7 @@ class NeuralNet :
             elif self.metric == "MPE" :
                 Metricc = 100*( abs((anodes[-1] - yy)/yy) ).sum()/len(yy)
             elif self.metric == "R2" :
-                st.write(yy)
+                st.write(yy, anodes[-1])
                 Metricc = r2_score(yy, anodes[-1] )
             else :
                 raise ValueError("Misspelled or inappropriate metric for %s" % self.task)
