60cc90d50919847afa175b1bb32c025f9008d582
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-03 08:45:06 +0100

    Update Function_Categoric_to_numeric.py

--- functions/Function_Categoric_to_numeric.py
+++ functions/Function_Categoric_to_numeric.py
@@ -13,43 +13,42 @@ def Categoric_to_numeric(dataframe, categorical_features, step_further) :
     - step_further: flag di avanzamento
     La funzione restituisce i seguenti risultati:
     - dataframe modificato
-    - Tra_categ_list: lista con informazioni relative al metodo utilizzato
+    - list_transformation: lista con informazioni relative al metodo utilizzato
     - step_further: step di avanzamento
     """
 
     # Menu dropdown per la scelta del metodo di conversione
-    Tra_categ = st.selectbox(
-        'How would you like to transform categorical features?', ['','OneHotEncoder','String to numbers'])
+    menu_transformation = st.selectbox( 'How would you like to transform categorical features?', ['','OneHotEncoder','String to numbers'] )
 
     # Gestione delle varie scelte
-    if Tra_categ == '' :
-        jobs_encoder = None
-        pass
-    elif Tra_categ == 'OneHotEncoder':
-        jobs_encoder = LabelBinarizer()
-        for i in categorical_features :
+    if menu_transformation == 'OneHotEncoder':
+        jobs_encoder = LabelBinarizer()                        # Definizione oggetto encoder
+        for i in categorical_features :                        # Procedure di rinominazione colonne
             columns = np.unique(dataframe[i].astype(str))
             m = 0
             for j in columns :
                 columns[m] = i + "_" + columns[m]
                 m = m + 1
+            # Trasformazione del dataframe
             transformed = pd.DataFrame(jobs_encoder.fit_transform(dataframe[i].astype(str)), columns = columns)
             dataframe = pd.concat([transformed, dataframe], axis=1).drop([i], axis = 1)
-
-    elif Tra_categ == 'String to numbers':
+    else :
         jobs_encoder = None
-        for i in categorical_features :
-            dataframe[i].replace(np.unique(dataframe[i]),np.arange(0,len(np.unique(dataframe[i]))),inplace = True)
+        if menu_transformation == '' :
+            pass
+        elif menu_transformation == 'String to numbers':
+            for i in categorical_features :
+                dataframe[i].replace(np.unique(dataframe[i]),np.arange(0,len(np.unique(dataframe[i]))),inplace = True)
 
     # Aggiornamento dello step
     step_further = 4
 
     # Possibilità di visualizzare il dataframe
-    if Tra_categ != '':
+    if menu_transformation != '':
         if st.checkbox('Show dataframe', key = 530):                
             st.write(dataframe)
 
     # Creazione lista per l'applicazione della pipeline finale
-    Tra_categ_list = [Tra_categ, jobs_encoder]
+    list_transformation = [menu_transformation, jobs_encoder]
 
-    return dataframe, Tra_categ_list, step_further
+    return dataframe, list_transformation, step_further
