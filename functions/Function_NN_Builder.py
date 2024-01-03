50230e55a85b9719a95d76437cd104cae300a954
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-03 09:36:26 +0100

    Update Function_NN_Builder.py

--- functions/Function_NN_Builder.py
+++ functions/Function_NN_Builder.py
@@ -101,7 +101,7 @@ def NN_Builder(dataframe) :
         else :
             Metrics_final = ["RMSE", "MAE", "MAPE", "R2"]
         Final_metric = st.selectbox(
-            'Choose the Evaluation metric (the metric will be used to evaluate the training and the validation sets)',
+            'fChoose the Evaluation metric (the metric will be used to evaluate the training and the validation sets)',
             Metrics_final)
 
     # Check del numero di activation functions e del numero di Hidden layers
