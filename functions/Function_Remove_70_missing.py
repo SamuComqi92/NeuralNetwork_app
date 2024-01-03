a6badf0b3ea9bf1c838b8478a61b73b1e0fac5c6
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-03 15:41:56 +0100

    Update Function_Remove_70_missing.py

--- functions/Function_Remove_70_missing.py
+++ functions/Function_Remove_70_missing.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Funzione per rimuovere le colonne e le righe con più del 70% di valori mancanti
 # Tali colonne potenzialmente non hanno nessuna informazione utile
 # In altri casi, tali colonne possono essere trasformate per conservare le poche informazioni (NON INCLUSO IN QUESTO SCRIPT)
-def remove_missing(dataframe) :
+def remove_missing(dataframe, threshold = 0.7) :
     """
     La funzione accetta come unico parametro il dataframe da controllare
     La funzione restituisce il dataframe ripulito da righe e colonne con più del 70% di valori mancanti, e la lista di colonne rimaste
@@ -13,18 +13,31 @@ def remove_missing(dataframe) :
     Rows = dataframe.shape[0]
     Columns = dataframe.shape[1]
 
+    # Rimozione delle colonne con più del 70% di valori mancanti
+    removed_columns = [col for col in dataframe.columns if dataframe[col].isna().mean() > threshold]
+    dataframe.drop(columns = removed_columns, inplace = True)
+
+    # Rimozione delle righe con più del 70% di valori mancanti
+    row_threshold = int( dataframe.shape[1] * (1 - threshold) )
+    dataframe.dropna(thresh = row_threshold, inplace = True)
+
+    
     # Rimozione delle colonne con più del 70% di valori mancanti    
-    Removed_columns = []
-    for i in dataframe.columns :
-        if dataframe[i].isna().sum()/Rows > 0.7 :
-            Removed_columns.append(i)
-            dataframe.drop([i], axis = 1, inplace = True)
+    #Removed_columns = []
+    #for i in dataframe.columns :
+    #    if dataframe[i].isna().sum()/Rows > 0.7 :
+    #        Removed_columns.append(i)
+    #        dataframe.drop([i], axis = 1, inplace = True)
 
     # Rimozione delle righe con più del 70% di valori mancanti
     # Ogni riga che rappresenta un record, se ha pochi valori non ha nessuna informazione utile
-    dataframe.dropna(thresh = int(Columns*0.7), inplace = True)
-    dataframe = dataframe.reset_index()
-    dataframe.drop("index", axis = 1, inplace = True)
-    Selected_columns_start = dataframe.columns
+    #dataframe.dropna(thresh = int(Columns*0.7), inplace = True)
+    #dataframe = dataframe.reset_index()
+    #dataframe.drop("index", axis = 1, inplace = True)
+    #Selected_columns_start = dataframe.columns
 
-    return dataframe, Selected_columns_start
+    # Rest index + colonne finali
+    dataframe.reset_index(drop = True, inplace = True)
+    selected_columns = dataframe.columns
+    
+    return dataframe, selected_columns
