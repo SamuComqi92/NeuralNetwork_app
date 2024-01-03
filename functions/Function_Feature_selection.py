1d97134d47fd321bcb7fd2e94245a5062e6fa3f0
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-03 08:47:41 +0100

    Update Function_Feature_selection.py

--- functions/Function_Feature_selection.py
+++ functions/Function_Feature_selection.py
@@ -10,31 +10,31 @@ def Feature_selection(dataframe) :
     """
     # Check per controllare che il dataframe non sia vuoto + selezione della colonna target
     if dataframe.empty == False :
-        Tar = st.multiselect('Select the target:', dataframe.columns, key = 100)
+        target = st.multiselect('Select the target:', dataframe.columns, key = 100)
 
     # Lista colonne
-    Lista_col = dataframe.drop(Tar,axis = 1).columns.tolist()
-    Lista_col.insert(0,'All')
-    Lista_col.insert(0,'None')
+    Lista_col = dataframe.drop(target, axis = 1).columns.tolist()
+    Lista_col.insert(0, 'All')
+    Lista_col.insert(0, 'None')
 
     # Menu dropdown per multiselezione delle colonne categoriche
-    Categ = st.multiselect("Select categorical features:", Lista_col, key = 1)
-    if "All" in Categ:
-        Categ = dataframe.drop(Tar, axis = 1).columns.tolist()
-    elif "None" in Categ :
+    categorical = st.multiselect("Select categorical features:", Lista_col, key = 1)
+    if "All" in categorical:
+        categorical = dataframe.drop(target, axis = 1).columns.tolist()
+    elif "None" in categorical :
         pass
 
     # Menu dropdown per multiselezione delle colonne numeriche
-    Numer = st.multiselect("Select numerical features:", Lista_col, key = 2)
-    if "All" in Numer:
-        Numer = dataframe.drop(Tar,axis = 1).columns.tolist()
-    elif "None" in Numer :
+    numerical = st.multiselect("Select numerical features:", Lista_col, key = 2)
+    if "All" in numerical:
+        numerical = dataframe.drop(target, axis = 1).columns.tolist()
+    elif "None" in numerical :
         pass
 
     # Conversione delle colonne numeriche e Target
     # Può capitare che dopo la lettura di un file CSV, i dati numerici (con decimali) non siano convertiti in modo corretto
-    for i in Numer+Tar :
+    for i in numerical + target :
         dataframe[i] = dataframe[i].astype(str).str.replace(',', '.').astype(float)
         dataframe[i].apply(pd.to_numeric)
 
-    return dataframe, Tar, Categ, Numer
+    return dataframe, target, categorical, numerical
