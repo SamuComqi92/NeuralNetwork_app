d5a873f4fa38459111f7d529b0d0cf52d098d381
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-03 12:31:00 +0100

    Update Function_Standard_X_train.py

--- functions/Function_Standard_X_train.py
+++ functions/Function_Standard_X_train.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 # La funzione trasforma il set di training in base alla scelta dell'utente, standardizzando tutti i valori numerici
-def Standard_X_train(dataframe, X_train, X_test, step_further) :
+def standardize_X_train(dataframe, X_train, X_test, step_further) :
     """
     La funzione accetta i seguenti argomenti:
     - dataframe: dataframe per controllare la presenza di valori mancanti
@@ -19,40 +19,35 @@ def Standard_X_train(dataframe, X_train, X_test, step_further) :
     - step_further
     - flag_stand: tipologia standardizzazione
     """
-    # Inizializzazione flag relative al metodo utilizzato
-    flag_stand = 0
 
-    # Check avanzamento e valori nulli
-    #if step_further == 5 and dataframe.isna().sum().sum() == 0:
     st.text("")
     st.text("")
     st.text("")
     st.write("### Standardization")
     st.write("The standardization defined from the Training set will be applied to the Test set")
 
+    # Inizializzazione flag relative al metodo utilizzato
+    flag_stand = 0
+
     # Menu dropdown per la scelta del motodo da utilizzare
     Tra_num = st.selectbox( 'How would you like to normalize data?', ['','MinMaxScaler','StandardScaler','Do not normalize'] )
 
     # Gestione dei vari casi
-    if Tra_num == '' :
+    if Tra_num == '' :                                        # L'app non va avanti se non viene selezionato un valore (grazie a step_further non aggiornaot)
         pass
     elif Tra_num == 'MinMaxScaler':
-        flag_stand = 1
-        step_further = 6
         Set_scaler = MinMaxScaler()
         Set_scaler.fit(X_train)
         X_train = pd.DataFrame(Set_scaler.transform(X_train), columns = X_train.columns)
         X_test = pd.DataFrame(Set_scaler.transform(X_test), columns = X_test.columns)
+        flag_stand, step_further = 1, 6
     elif Tra_num == 'StandardScaler':
-        flag_stand = 2
-        step_further = 6
         Set_scaler = StandardScaler()
         Set_scaler.fit(X_train)
         X_train = pd.DataFrame(Set_scaler.transform(X_train), columns = X_train.columns)
         X_test = pd.DataFrame(Set_scaler.transform(X_test), columns = X_test.columns)
+        flag_stand, step_further = 2, 6
     elif Tra_num == 'Do not normalize':
-        flag_stand = 3
-        step_further = 6
-        pass
+        flag_stand, step_further = 3, 6
 
     return X_train, X_test, Tra_num, step_further, flag_stand
