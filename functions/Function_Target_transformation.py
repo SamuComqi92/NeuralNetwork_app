ccdb0e451f93aac3f33154e665c23df2798bf28e
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-03 12:28:11 +0100

    Update Function_Target_transformation.py

--- functions/Function_Target_transformation.py
+++ functions/Function_Target_transformation.py
@@ -20,8 +20,7 @@ def target_transformation(dataframe, target, y_train, y_test, step_further) :
     - step_further
     """
     # Inizializzazione dei flag relativi al tipo di normalizzazione usata e al tipo di trasformazione (10: log, 20: minmax)
-    flag_norm = 0
-    flag_transf = 0
+    flag_norm, flag_transf = 0, 0
 
     # Menu drop down per scegliere se trasformare o meno la colonna target
     flag_transformation = st.selectbox( 
@@ -32,32 +31,25 @@ def target_transformation(dataframe, target, y_train, y_test, step_further) :
     if flag_transformation == '':                    # L'app non va avanti se non si seleziona un metodo (questo grazio allo step_further non aggiornato)
         target_minmax = None
     elif flag_transformation == 'No' :
-        target_minmax = None
-        step_further = 7
+        target_minmax, step_further = None, 7
     else :
         # Log(x+1) transformation
         if float(dataframe[target].min()) >= 0. :                
-            target_minmax = None
-            step_further = 7
+            target_minmax, step_further = None, 7
             if float(dataframe[target].max()) > 1. :
-                y_train = np.log10(y_train + 1)
-                y_test = np.log10(y_test + 1)
+                y_train, y_test = np.log10(y_train + 1), np.log10(y_test + 1)
                 st.write("Target values have been transformed with Log(x+1)")
-                flag_norm = 1
-                flag_transf = 10
+                flag_norm, flag_transf = 1, 10
             else :
                 st.write("Target values are already in a range between 0 and 1")
                 flag_norm = 2
         # MinMax transformation
         elif float(dataframe[target].min()) < 0. :
-            st.write("MinMaxScaler will be used (reducing the target values in a range between 0 and 1)")
+            st.write("Target values have been reduced in a range between 0 and 1 using the MinMaxScaler")
             target_minmax = MinMaxScaler()
             target_minmax.fit(y_train)
-            y_train = target_minmax.transform(y_train)
-            y_test = target_minmax.transform(y_test)
-            flag_norm = 3
-            flag_transf = 20
-            step_further = 7
+            y_train, y_test = target_minmax.transform(y_train), target_minmax.transform(y_test)
+            flag_norm, flag_transf, step_further = 3, 20, 7
     
     # Creo lista per l'applicazione finale della pipeline
     Norm_tar_list = [flag_transformation, target_minmax, flag_norm, flag_transf]
