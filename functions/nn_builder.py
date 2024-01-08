1d0ab8df19602a63bbe0beaee0392b413f86c036
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-08 11:52:34 +0100

    Update nn_builder.py

--- functions/nn_builder.py
+++ functions/nn_builder.py
@@ -32,7 +32,7 @@ def nn_builder(dataframe, Task) :
     st.markdown(html_str, unsafe_allow_html=True)
 
     # Main parameters
-    st.write("These are the main parameters of your Neural Network (you can leave their default values).")
+    st.write("These are the main parameters of your Neural Network (you may leave their default values).")
     left_column, right_column = st.columns(2)            # Nella parte principale, crea due colonne dove posso sistemare testi e bottoni
     with left_column:                                    # Qui scelgo di scrivere cose solo nela parte destra
         st.write('Hidden layers and units')
@@ -54,37 +54,6 @@ def nn_builder(dataframe, Task) :
         Final_metric = st.selectbox(
             'Choose the Evaluation metric (for the evaluation of the training and the validation sets).',
             Metrics_final)
-        
-        # st.text("")
-        # st.text("")
-        # Algo = st.selectbox(
-        #     'Optimization algorithm',
-        #     ["Batch","Adam"]
-        #     )
-
-        
-
-        # st.text("")
-        # st.text("")
-        # Regularization = st.selectbox(
-        #     'Type of Regularization',
-        #     ["None","Ridge","Lasso"])
-
-        # st.text("")
-        # st.text("")
-        # Momentum = st.text_input('The momentum factor in the weights optimization', '0')
-
-        # st.text("")
-        # st.text("")
-        # Early_stopping = st.selectbox(
-        #     'A flag to apply Early-stopping to the algorithm (for Adam, it is better to set it to "False")',
-        #     ["False","True"])
-
-        # st.text("")
-        # st.text("")
-        # Verbose = st.selectbox(
-        #     'A flag to display results while processing (0: do not deplay, 1: deplay)',
-        #     [0, 1])
 
     with right_column :
         st.write('Number of Iterations')
@@ -100,30 +69,64 @@ def nn_builder(dataframe, Task) :
         st.text("")
         st.write('Random State')
         Random_state = st.text_input('A random state value for reproducible results', '0')
-        
-        # st.text("")
-        # st.text("")
-        # Batch = st.selectbox(
-        #     'Size of mini-batches (0: all data will be used)',
-        #     np.arange(0,len(dataframe))
-        #     )
 
-        # st.text("")
-        # st.text("")
-        # Decay = st.text_input('Decay parameter for the Learning rate (0 by default)','0')
-
-        # st.text("")
-        # st.text("")
-        # Lambda = st.text_input('Regularization factor Lambda (0 by default)', '0')
+    
+    # Other parameters
+    st.write("These are other advanced parameters (you may leave their default values).")
+    left_column2, right_column2 = st.columns(2)            # Nella parte principale, crea due colonne dove posso sistemare testi e bottoni
+    with left_column2:   
+        st.text("")
+        st.text("")
+        Algo = st.selectbox(
+            'Optimization algorithm',
+            ["Batch","Adam"]
+            )
 
         
 
-        # st.text("")
-        # st.text("")
-        # Patient = st.text_input('The number of epochs to check for early stopping (it occurs in case when Adam, and/or Momentum are applied)', '5')
+        st.text("")
+        st.text("")
+        Regularization = st.selectbox(
+            'Type of Regularization',
+            ["None","Ridge","Lasso"])
+
+        st.text("")
+        st.text("")
+        Momentum = st.text_input('The momentum factor in the weights optimization', '0')
+
+        st.text("")
+        st.text("")
+        Early_stopping = st.selectbox(
+            'A flag to apply Early-stopping to the algorithm (for Adam, it is better to set it to "False")',
+            ["False","True"])
+
+        st.text("")
+        st.text("")
+        Verbose = st.selectbox(
+            'A flag to display results while processing (0: do not deplay, 1: deplay)',
+            [0, 1])
+    with right_column2 :
+        st.text("")
+        st.text("")
+        Batch = st.selectbox(
+            'Size of mini-batches (0: all data will be used)',
+            np.arange(0,len(dataframe))
+            )
+
+        st.text("")
+        st.text("")
+        Decay = st.text_input('Decay parameter for the Learning rate (0 by default)','0')
+
+        st.text("")
+        st.text("")
+        Lambda = st.text_input('Regularization factor Lambda (0 by default)', '0')
 
         
 
+        st.text("")
+        st.text("")
+        Patient = st.text_input('The number of epochs to check for early stopping (it occurs in case when Adam, and/or Momentum are applied)', '5')
+    
     # Check del numero di activation functions e del numero di Hidden layers
     # Se scelgo più di una funzione di attivazione, il numero complessivo deve essere uguale al numero di layer della Rete
     if len(Function_) < len(Hidden_layers) :
