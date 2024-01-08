7c2df02e8c35e2150937e3563e28398852e4ba74
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-08 11:33:46 +0100

    Update NN_upload_and_traning.py

--- NN_upload_and_traning.py
+++ NN_upload_and_traning.py
@@ -97,7 +97,7 @@ if uploaded_file is not None:
         # Viene data la possibilità di convertire i valori in due modi diversi    
         # Check dello step, della presenza di colonne categoriche e dell'assenza di valori mancanti (altrimenti, lo script si interrompe)
         st.write(Categ)
-        if (step_further == 3) and (len(Categ) != 0 or Categ != None) and (dataframe.isna().sum().sum() == 0) :
+        if (step_further == 3) and (len(Categ) != 0 or Categ != [None]) and (dataframe.isna().sum().sum() == 0) :
             dataframe, Tra_categ_list, step_further = categoric_to_numeric.categoric_to_numeric(dataframe, Categ, step_further)
         elif (dataframe.isna().sum().sum() != 0) :
             st.write("Something's wrong with the data. Missing values are still there...")
