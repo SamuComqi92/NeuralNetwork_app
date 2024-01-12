a8f69c5886de89306515befdc483929a639bc494
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-12 19:47:25 +0100

    Update footer.py

--- functions/footer.py
+++ functions/footer.py
@@ -24,7 +24,7 @@ def footer(*args):
                        opacity = 1,
                        display="flex",
                        justify_content="center",
-                       font_size=12
+                       font_size=8
                        )
     style_hr = styles(display = "block",
                       margin = px(8, 8, "auto", "auto"),
