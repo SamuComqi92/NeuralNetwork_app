1738bd910d76e7530928e209cbbf86c7c5ed295c
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-14 14:44:57 +0100

    Update imputation_process.py

--- functions/imputation_process.py
+++ functions/imputation_process.py
@@ -18,54 +18,40 @@ def imputation(dataframe, categorical, numerical) :
     - Array con i valori eliminati dal dataframe (se si è scelto tale metodo)
     """
 
-    # CATEGORICAL
-    # Inizializzazione dello step_further e del flag relativo al metodo di sostituzione
-    step_further, categ_impute, numer_impute = 0, 0, 0
-
-    # Controllo che ci siano valori mancanti nel dataframe e che ci siano colonne numeriche
-    if categorical and dataframe.isna().sum().sum() != 0:
-        
-        # Scelta del metodo di imputation
-        categorical_method = st.selectbox(
+    # Categorical features
+    step_further, categ_impute, numer_impute = 0, 0, 0                                                # Inizializzazione dello step_further e del flag relativo al metodo di sostituzione                            
+    if categorical and dataframe.isna().sum().sum() != 0:                                             # Controllo che ci siano valori mancanti nel dataframe e che ci siano colonne numeriche
+        categorical_method = st.selectbox(                                                            # Scelta del metodo di imputation
             'Which imputation method (for NA values) do you like best for categorical features?',
             ['','Substitute null values with string NAN','Substitute null values with the mode','Drop rows (careful!)'])
 
-        # Gestione delle varia casistiche
-        if categorical_method == 'Drop rows (careful!)' :                             # Rimozione delle righe con valori mancanti
-            value_imputation_cat = None
-            a = dataframe[categorical].isnull().to_numpy().nonzero()[0]               # Righe eliminate     
-            step_further, categ_impute = 1, 3
+        if categorical_method == 'Drop rows (careful!)' :                                             # Rimozione delle righe con valori mancanti
+            a = dataframe[categorical].isnull().to_numpy().nonzero()[0]                               # Righe eliminate     
+            value_imputation_cat, step_further, categ_impute = None, 1, 3
         else :
             a = np.array([])
-            if categorical_method == '' :                                             # Nessuna imputation (lo script non va avanti se non si sceglie un metodo)
-                value_imputation_cat = None                                           # La sospensione è dovuta allo step_further non aggiornato
-            elif categorical_method == 'Substitute null values with string NAN':      # Sostituzione con stringa "NAN"
+            if categorical_method == '' :                                                             # Nessuna imputation (lo script non va avanti se non si sceglie un metodo)
+                value_imputation_cat = None                                                           # La sospensione è dovuta allo step_further non aggiornato
+            elif categorical_method == 'Substitute null values with string NAN':                      # Sostituzione con stringa "NAN"
                 value_imputation_cat = "NAN"
                 dataframe[categorical] = dataframe[categorical].fillna(value_imputation_cat)
                 step_further, categ_impute = 1, 1
-            elif categorical_method == 'Substitute null values with the mode':        # Sostituzione con la moda
+            elif categorical_method == 'Substitute null values with the mode':                        # Sostituzione con la moda
                 value_imputation_cat = dataframe[categorical].mode().iloc[0]
                 dataframe[categorical] = dataframe[categorical].fillna(value_imputation_cat)
                 step_further, categ_impute = 1, 2
-
-    # Se non ci sono colonne categoriche
-    else :
+    else :                                                                                            # Se non ci sono colonne categoriche
         step_further, categorical_method, value_imputation_cat, a = 1, "", None, np.array([])
 
-    #--------------------------------------------------------------------------------------------------------------------------------------------
-    # NUMERICAL
-    # Controllo che ci siano valori mancanti nel dataframe e che ci siano colonne numeriche
-    if numerical and dataframe.isna().sum().sum() != 0:
-        # Scelta del metodo di imputation
-        numerical_method = st.selectbox(
+    # Numerical features
+    if numerical and dataframe.isna().sum().sum() != 0:                                              # Controllo che ci siano valori mancanti nel dataframe e che ci siano colonne numeriche
+        numerical_method = st.selectbox(                                                             # Scelta del metodo di imputation
             'Which imputation method (for NA values) do you like best for numerical features?',
             ['','Substitute null values with the mean','Substitute null values with the median','Drop rows (careful!)'])
 
-        # Gestione delle varia casistiche
         if numerical_method == 'Drop rows (careful!)' :                                              # Rimozione delle righe con valori mancanti
-            value_imputation_num = None
             b = dataframe[Numer].isnull().to_numpy().nonzero()[0]                                    # Righe eliminate
-            step_further, numer_impute = 2, 3
+            value_imputation_num, step_further, numer_impute = None, 2, 3
         else :
             b = np.array([])
             if numerical_method == '' :                                                              # Nessuna imputation
@@ -78,9 +64,7 @@ def imputation(dataframe, categorical, numerical) :
                 value_imputation_num = dataframe[numerical].median()
                 dataframe[numerical] = dataframe[numerical].fillna(value_imputation_num)
                 step_further, numer_impute = 2, 2
-    
-    # Se non ci sono colonne numeriche
-    else :
+    else :                                                                                           # Se non ci sono colonne numeriche
         step_further, numerical_method, value_imputation_num, b = 2, "", None, np.array([])  
 
     # Creo liste dei parametri che mi serviranno nell'applicazione della pipeline finale
