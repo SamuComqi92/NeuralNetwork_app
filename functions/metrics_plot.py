6114770d5c2b059ef7eebb452da30b9fc1fe00ec
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-20 16:07:07 +0100

    Update metrics_plot.py

--- functions/metrics_plot.py
+++ functions/metrics_plot.py
@@ -64,13 +64,13 @@ def metrics_plot(Model, X_train, X_test, y_train, y_test, Task, Norm_tar_list, F
         other_models = [ LogisticRegression(), DecisionTreeClassifier(), RandomForestClassifier(n_estimators = 100, random_state = 42) ]
         acc_models, pre_models, rec_models, f1_models = [], [], [], []
         for model in other_models :
-            st.write(model)
             model.fit(X_train, y_train)
             predictions = model.predict(X_test)
             acc_models.append( accuracy_score(y_test, predictions) )
             pre_models.append( precision_score(y_test, predictions, average = "weighted") )
             rec_models.append( recall_score(y_test, predictions, average = "weighted") )
             f1_models.append( f1_score(y_test, predictions, average = "weighted") )
+            st.write(acc_models, pre_models, rec_models, f1_models)
         other_results = pd.DataFrame( [acc_models, pre_models, rec_models, f1_models],  columns = Col_final, index = ID_final ) 
         st.write(other_results)
         
