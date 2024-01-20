6b5f3e5b2808041d3831e83b39ec2b7641fe7190
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-20 15:47:20 +0100

    Update NN_upload_and_traning.py

--- NN_upload_and_traning.py
+++ NN_upload_and_traning.py
@@ -148,9 +148,9 @@ if uploaded_file is not None:                                                 # 
                         model.fit(X_train, y_train)
                         predictions = model.predict(X_test)
                         accuracy_models.append( accuracy_score(y_test, predictions) )
-                    st.write( pd.DataFrame(accuracy_models, columns = ["Logistic regression", "Random Forest", "Decision Tree"]) )
+                    st.write( pd.DataFrame(accuracy_models, index = ["Logistic regression", "Random Forest", "Decision Tree"]).T )
                 elif Task1 == 'Regression':
-                    model = RandomForestRegressor(n_estimators=100, random_state=42)
+                    model = RandomForestRegressor(n_estimators = 100, random_state = 42)
                     model.fit(X_train, y_train)
                     predictions = model.predict(X_test)
                     mse = mean_squared_error(y_test, predictions)
