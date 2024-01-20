5df8902a31bc38739b8eeda0fb1dec687da112ee
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-01-20 16:10:52 +0100

    Update metrics_plot.py

--- functions/metrics_plot.py
+++ functions/metrics_plot.py
@@ -66,12 +66,11 @@ def metrics_plot(Model, X_train, X_test, y_train, y_test, Task, Norm_tar_list, F
         for model in other_models :
             model.fit(X_train, y_train)
             predictions = model.predict(X_test)
-            st.write(predictions)
-            acc_models.append( accuracy_score(y_test, predictions) )
-            #pre_models.append( precision_score(y_test, predictions, average = "weighted") )
+            #acc_models.append( accuracy_score(y_test, predictions) )
+            pre_models.append( precision_score(y_test, predictions, average = "weighted") )
             #rec_models.append( recall_score(y_test, predictions, average = "weighted") )
             #f1_models.append( f1_score(y_test, predictions, average = "weighted") )
-            st.write(acc_models)
+        st.write(pre_models)
         #other_results = pd.DataFrame( [acc_models, pre_models, rec_models, f1_models],  columns = Col_final, index = ID_final ).T
         #st.write(other_results)
         
